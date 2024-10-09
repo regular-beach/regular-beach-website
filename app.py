62c280a839a0fcba6a64a07599ffe7d3cb2b6019
regular beach <renconger@gmail.com>
2024-10-09 13:46:29 -0700

    first draft

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+  
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
+
+
+    
