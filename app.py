ae9c8e37dd48448b7346d1b227e9d676682ff1ad
regular beach <renconger@gmail.com>
2024-10-30 14:29:27 -0700

    ...

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def inject_projects():
 #if __name__ == '__main__':
  #app.run(host='0.0.0.0') 
   
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+    #serve(app, host="0.0.0.0", port=8000)
 
 
     
