e4df2cec1bf4704a5ec24e18dc7ac695ca76397a
regular beach <renconger@gmail.com>
2025-12-06 15:29:18 -0800

    t1

--- app.py
+++ app.py
@@ -139,11 +139,11 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 
-#if __name__ == '__main__':
-# app.run(host='0.0.0.0') 
+if __name__ == '__main__':
+ app.run(host='0.0.0.0') 
   
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+    #serve(app, host="0.0.0.0", port=8000)
 
 
     
